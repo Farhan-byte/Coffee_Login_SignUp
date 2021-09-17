14865e32b53b136abbcd67b4d8eeac624c555b1d
Farhan-byte <fari30456@gmail.com>
2021-09-17 23:03:03 +0500

    UI of CoffeeApp

---
+++ lib/screens/new_account.dart
@@ -0,0 +1,105 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:coffee_app/pallete.dart';
+import 'package:coffee_app/widgets/widgets.dart';
+
+class CreateNewAccount extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        BackgroundImage(image: 'assets/images/peakpx.jpg'),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.width * 0.1,
+                ),
+                Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top:110),
+                      child: Center(
+                        child: Text(
+                      'CoffeeApp',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold),
+                  ),
+                      ),
+                    ),
+                    
+                  ],
+                ),
+                SizedBox(
+                  height: size.width * 0.3,
+                ),
+                Column(
+                  children: [
+                    TextInputField(
+                      icon: FontAwesomeIcons.user,
+                      hint: 'User',
+                      inputType: TextInputType.name,
+                      inputAction: TextInputAction.next,
+                    ),
+                    TextInputField(
+                      icon: FontAwesomeIcons.envelope,
+                      hint: 'Email',
+                      inputType: TextInputType.emailAddress,
+                      inputAction: TextInputAction.next,
+                    ),
+                    PasswordInput(
+                      icon: FontAwesomeIcons.lock,
+                      hint: 'Password',
+                      inputAction: TextInputAction.next,
+                    ),
+                    PasswordInput(
+                      icon: FontAwesomeIcons.lock,
+                      hint: 'Confirm Password',
+                      inputAction: TextInputAction.done,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    RoundedButton(buttonName: 'Register'),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?',
+                          style: kBodyText,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/');
+                          },
+                          child: Text(
+                            'Login',
+                            style: kBodyText.copyWith(
+                                color: kBlue, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'password_input.dart';
+export 'textfield_input.dart';
+export 'rounded_btn.dart';
+export 'background_image.dart';
